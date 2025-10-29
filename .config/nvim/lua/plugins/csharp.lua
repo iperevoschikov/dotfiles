206d999513e54270e229bbf6827225337ce994b3
iperevoschikov <iperevoschikov@skbkontur.ru>
2025-10-29 11:10:27 +0500

    wip

--- .config/nvim/lua/plugins/csharp.lua
+++ .config/nvim/lua/plugins/csharp.lua
@@ -1,20 +1,9 @@
 return {
   {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        omnisharp = {
-          cmd = { "omnisharp", "--languageserver", "--hostPID", tostring(vim.fn.getpid()) },
-          enable_editorconfig_support = true,
-          enable_ms_build_load_projects_on_demand = true,
-          enable_roslyn_analyzers = true,
-          organize_imports_on_format = true,
-          enable_import_completion = true,
-          sdk_include_prereleases = true,
-          analyze_open_documents_only = false,
-        },
-      },
-    },
+    "seblyng/roslyn.nvim",
+    ---@module 'roslyn.config'
+    ---@type RoslynNvimConfig
+    opts = {},
   },
   {
     "stevearc/conform.nvim",
