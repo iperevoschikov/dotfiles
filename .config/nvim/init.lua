1d8154b46ce52a4261dc4a2ca745e710a7db5038
iperevoschikov <iperevoschikov@skbkontur.ru>
2026-02-27 10:46:35 +0500

    feat(mason): remove custom registries

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-require("mason").setup({
-  registries = {
-    "github:mason-org/mason-registry",
-    "github:Crashdummyy/mason-registry",
-  },
-})
