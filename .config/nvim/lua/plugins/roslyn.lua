5520f72c88920feb479fe4525bdc8fc5c592ce61
iperevoschikov <iperevoschikov@skbkontur.ru>
2026-05-08 14:01:10 +0500

    feat(nvim): disable all lsp except roslyn

--- .config/nvim/lua/plugins/roslyn.lua
+++ .config/nvim/lua/plugins/roslyn.lua
@@ -1,8 +1,19 @@
 return {
-  "seblyng/roslyn.nvim",
-  ---@module 'roslyn.config'
-  ---@type RoslynNvimConfig
-  opts = {
-    -- your configuration comes here; leave empty for default settings
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        omnisharp = false,
+        csharp_ls = false,
+      },
+    },
+  },
+  {
+    "seblyng/roslyn.nvim",
+    ---@module 'roslyn.config'
+    ---@type RoslynNvimConfig
+    opts = {
+      -- your configuration comes here; leave empty for default settings
+    },
   },
 }
