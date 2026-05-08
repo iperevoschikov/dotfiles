5b8cbb0256c46bdcb358c58dba2cae93785afc57
iperevoschikov <iperevoschikov@skbkontur.ru>
2026-05-08 13:22:44 +0500

    feat(nvim): trying roslyn lsp and overseer for dotnet tasks

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+require("mason").setup({
+  registries = {
+    "github:mason-org/mason-registry",
+    "github:Crashdummyy/mason-registry",
+  },
+})
